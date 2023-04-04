d3436eb3f820d635a406d384b90d39e074869e30
boblightning <satrianugraha4000@gmail.com>
2023-04-04 21:28:37 +0700

    apr4

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -21,47 +21,46 @@ $this->db->update('buku', $data, $where);
 }
 public function hapusBuku($where = null)
 {
-$this->db->delete('buku', $where);
-}
-public function total($field, $where)
-{
-$this->db->select_sum($field);
-if(!empty($where) && count($where) > 0){
-$this->db->where($where);
-}
-$this->db->from('buku');
-return $this->db->get()->row($field);
-}
-//manajemen kategori
-public function getKategori()
-{
-return $this->db->get('kategori');
-}
-public function kategoriWhere($where)
-{
-return $this->db->get_where('kategori', $where);
-}
-public function simpanKategori($data = null)
-{
-$this->db->insert('kategori', $data);
-}
-public function hapusKategori($where = null)
-{
-$this->db->delete('kategori', $where);
-}
-public function updateKategori($where = null, $data = null)
-{
-$this->db->update('kategori', $data, $where);
-}
-//join
-public function joinKategoriBuku($where)
-{
-$this->db->select('buku.id_kategori,kategori.kategori');
-$this->db->from('buku');
-$this->db->join('kategori','kategori.id
-=
-buku.id_kategori');
-$this->db->where($where);
-return $this->db->get();
-}
-}
+    $this->db->delete('buku', $where);
+    }
+    public function total($field, $where)
+    {
+    $this->db->select_sum($field);
+    if(!empty($where) && count($where) > 0){
+    $this->db->where($where);
+    }
+    $this->db->from('buku');
+    return $this->db->get()->row($field);
+    }
+    //manajemen kategori
+    public function getKategori()
+    {
+    return $this->db->get('kategori');
+    }
+    public function kategoriWhere($where)
+    {
+    return $this->db->get_where('kategori', $where);
+    }
+    public function simpanKategori($data = null)
+    {
+    $this->db->insert('kategori', $data);
+    }
+    public function hapusKategori($where = null)
+    {
+    $this->db->delete('kategori', $where);
+    }
+    public function updateKategori($where = null, $data = null)
+    {
+    $this->db->update('kategori', $data, $where);
+    }
+    //join
+    public function joinKategoriBuku($where)
+    {
+        $this->db->select('buku.id_kategori,kategori.kategori');
+        $this->db->from('buku');
+        $this->db->join('kategori','kategori.id =
+        buku.id_kategori');
+        $this->db->where($where);
+        return $this->db->get();
+        }
+        }
