23b99c751f017458b1b6be115e6fe1b49f35916b
boblightning <satrianugraha4000@gmail.com>
2023-05-02 21:14:24 +0700

    update login with error templates/header.php

--- application/views/templates/aute_footer.php
+++ application/views/templates/aute_footer.php
@@ -4,8 +4,7 @@
 <script src="<?= base_url('assets/');
 ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- Core plugin JavaScript-->
-<script src="<?= base_url('assets/'); ?>vendor/jquery-
-easing/jquery.easing.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 <!-- Custom scripts for all pages-->
 <script src="<?= base_url('assets/'); ?>js/sb-admin-
 2.min.js"></script>
@@ -13,4 +12,5 @@ easing/jquery.easing.min.js"></script>
 $('.alert-message').alert().delay(3000).slideUp('slow');
 </script>
 </body>
+
 </html>
